4b4001b12796975116d54c1f54cc36a56125358a
Fenroe <scrambles9494@gmail.com>
2021-08-05 16:47:05 +0300

    fixed setup errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 
  module.exports = {
+   mode: 'development',
    entry: './src/index.js',
    output: {
      filename: 'bundle.js',
@@ -23,35 +21,6 @@ const json5 = require('json5');
          test: /\.(woff|woff2|eot|ttf|otf)$/i,
          type: 'asset/resource',
        },
-       {
-         test: /\.(csv|tsv)$/i,
-         use: ['csv-loader'],
-       },
-       {
-         test: /\.xml$/i,
-         use: ['xml-loader'],
-       },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
      ],
    },
  };
